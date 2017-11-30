a90d957bf03a8e305ffb6f6b3b255afd869f52f8
Adarsh Pavani <adarsh.pavani@mapbox.com>
2017-11-29 17:16:23 -0800

    titles face the camera now

--- Assets/Scripts/CameraLookAt.cs
+++ Assets/Scripts/CameraLookAt.cs
@@ -11,6 +11,6 @@ public class CameraLookAt : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.LookAt(-Camera.main.transform.position,Vector3.up);
+		transform.LookAt(2*transform.position - Camera.main.transform.position);
 	}
 }
